0cd5fc305cec0877cb9dc1453a8265b89d712d55
Hoanh An <hoanhan101@gmail.com>
2018-01-15 13:24:48 -0500

    update path

--- setup.sh
+++ setup.sh
@@ -10,25 +10,22 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $DIR
-echo "$DIR/.vimrc"
-
 if [[ "$(uname -s)" == "Darwin" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
     brew install macvim --override-system-vim
     brew install tmux
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    cp "$DIR/.vimrc" ~
-    cp "$DIR/.tmux.conf" ~
+    cp "$DIR/dev-setup/.vimrc" ~
+    cp "$DIR/dev-setup/.tmux.conf" ~
 	vim +PluginInstall +qall
 else
 	sudo apt-get update
 	sudo apt-get install vim
     sudo apt-get install tmux
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    cp "$DIR/.vimrc" ~
-    cp "$DIR/.tmux.conf" ~
+    cp "$DIR/dev-setup/.vimrc" ~
+    cp "$DIR/dev-setup/.tmux.conf" ~
 	vim +PluginInstall +qall
 fi
 
