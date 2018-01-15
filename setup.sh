cc545e4f07e19e78c4c1e7b9fa05e6810aaa6338
Hoanh An <hoanhan101@gmail.com>
2018-01-15 13:42:45 -0500

    update path and debugging comment

--- setup.sh
+++ setup.sh
@@ -8,25 +8,36 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 if [[ "$(uname -s)" == "Darwin" ]]; then
+    echo "Install Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
+
+    echo "Install macvim"
     brew install macvim --override-system-vim
+
+    echo "Install tmux"
     brew install tmux
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    cp "$DIR/dev-setup/.vimrc" ~
-    cp "$DIR/dev-setup/.tmux.conf" ~
-	vim +PluginInstall +qall
 else
 	sudo apt-get update
+
+    echo "Install macvim"
 	sudo apt-get install vim
+
+    echo "Install tmux"
     sudo apt-get install tmux
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    cp "$DIR/dev-setup/.vimrc" ~
-    cp "$DIR/dev-setup/.tmux.conf" ~
-	vim +PluginInstall +qall
 fi
+
+echo "Install Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Copy .vimrc to root directory"
+cp "$PWD/.vimrc" ~
+
+echo "Copy .tmux.conf to root directory"
+cp "$PWD/.tmux.conf" ~
+
+echo "Install Plugins via Vundle"
+vim +PluginInstall +qall
 
 echo "Done"
