6e216612672eb95cff59069f160d4cc7f09e3050
Hoanh An <hoanhan101@gmail.com>
2018-01-15 14:20:46 -0500

    format styling

--- setup.sh
+++ setup.sh
@@ -6,38 +6,47 @@
 #   Date: 1/13/18
 # -------------------------------------------------------------
 
+echo "============================================================="
+echo "*                    BEGIN INSTALLING                       *"
+echo "-------------------------------------------------------------"
+
+echo "-------------------------------------------------------------"
+echo "*                       VIM & TMUX                          *"
+echo "-------------------------------------------------------------"
+
 set -e
 
 if [ "$(uname -s)" == "Darwin" ]; then
-    echo "Install Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
-
-    echo "Install macvim"
     brew install macvim --override-system-vim
-
-    echo "Install tmux"
     brew install tmux
 else
-	sudo apt-get update
-
-    echo "Install macvim"
-	sudo apt-get install vim
-
-    echo "Install tmux"
+    sudo apt-get update
+    sudo apt-get install vim
     sudo apt-get install tmux
 fi
 
-echo "Install Vundle"
+echo "-------------------------------------------------------------"
+echo "*                        VUNDLE                             *"
+echo "-------------------------------------------------------------"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "Copy .vimrc to root directory"
+echo "-------------------------------------------------------------"
+echo "*                   COPY .VIMRC TO ROOT                     *"
+echo "-------------------------------------------------------------"
 cp "$PWD/.vimrc" ~
 
-echo "Copy .tmux.conf to root directory"
+echo "-------------------------------------------------------------"
+echo "*                  COPY .TMUX.CONF TO ROOT                  *"
+echo "-------------------------------------------------------------"
 cp "$PWD/.tmux.conf" ~
 
-echo "Install Plugins via Vundle"
+echo "-------------------------------------------------------------"
+echo "*                 INSTALL PLUGINS VIA VUNDLE                 *"
+echo "-------------------------------------------------------------"
 vim +PluginInstall +qall
 
-echo "Done"
+echo "-------------------------------------------------------------"
+echo "*              INSTALL COMPLETE - READY TO USE              *"
+echo "============================================================="
