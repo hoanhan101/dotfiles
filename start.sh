b8bd4b1188bc65f229873ca81f7bb679bac480d3
Hoanh An <hoanhan@bennington.edu>
2018-04-25 01:49:15 -0400

    make echo colorful

--- start.sh
+++ start.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 
-# start.sh - Load vim and tmux configurations
+# start.sh - Load configurations to a Docker image
 # Author: Hoanh An (hoanhan@bennington.edu)
 # Date: 04/24/18
 
-echo "Begin the process"
+# Cyan '=>' symbol
+SYMBOL='\033[0;36m=>\033[0m'
 
-echo "Create git identity"
+echo "${SYMBOL} Begin the process"
+
+echo "${SYMBOL} Create git identity"
 git config --global credential.helper cache
 git config --global user.email "hoanhan@bennington.edu"
 git config --global user.name "Hoanh An"
 
-echo "Install Vundle and base plugins"
+echo "${SYMBOL} Install Vundle and base plugins"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp "$PWD/.plugins.vim" ~/.vimrc
 vim +PluginInstall +qall
 
-echo "Install additional configurations"
+echo "${SYMBOL} Install additional configurations"
 cp "$PWD/.vimrc" ~
 vim +PluginInstall +qall
 python3 ~/.vim/bundle/YouCompleteMe/install.py
 
-echo "Load tmux configurations"
+echo "${SYMBOL} Load tmux configurations"
 cp "$PWD/.tmux.conf" ~
 
-echo "Ready to use"
+echo "${SYMBOL} Install oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "${SYMBOL} Load oh my zsh configurations"
+cp "$PWD/.zshrc" ~
+
+echo "${SYMBOL} Ready to use"
